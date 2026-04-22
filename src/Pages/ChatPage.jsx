4d353eca1630c2673687363baf490f152a419658
Mjansitha03 <mjansitha03@gmail.com>
2026-04-22 19:21:24 +0530

    completed

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -380,8 +380,8 @@ const ChatPage = () => {
         `}
         >
           {/* HEADER */}
-          <div className="border-b border-slate-800 bg-slate-900/60 backdrop-blur-md">
-            <div className="max-w-[800px] mx-auto px-3 sm:px-4 py-3 flex items-center">
+          <div className="border-b border-slate-800 bg-slate-900/60 backdrop-blur-md max-w-[900px]">
+            <div className="max-w-[900px] mx-auto px-3 sm:px-4 py-3 flex items-center">
               {selectedChat && (
                 <>
                   {/* BACK BUTTON */}
@@ -540,8 +540,8 @@ const ChatPage = () => {
 
           {/* INPUT */}
           {selectedChat && (
-            <div className="border-t border-slate-800 bg-slate-900/60 backdrop-blur-md">
-              <div className="max-w-[800px] mx-auto px-3 sm:px-4 py-2">
+            <div className="border-t border-slate-800 bg-slate-900/60 backdrop-blur-md max-w-[900px]">
+              <div className="max-w-[900px] mx-auto px-3 sm:px-4 py-2">
                 <MessageInput
                   onSend={handleSend}
                   selectedChat={selectedChat}
